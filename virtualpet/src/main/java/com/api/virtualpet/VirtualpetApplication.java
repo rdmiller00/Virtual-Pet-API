37090f1bd9561af2a161170f13a90b20edc0fedb
rdmiller00 <rd.miller0619@gmail.com>
2024-01-09 14:04:51 -0500

    initial framework_test commit

---
+++ virtualpet/src/main/java/com/api/virtualpet/VirtualpetApplication.java
@@ -0,0 +1,13 @@
+package com.api.virtualpet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VirtualpetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VirtualpetApplication.class, args);
+	}
+
+}
